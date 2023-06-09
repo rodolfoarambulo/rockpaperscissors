0805b11aec228d5ad3a82630da8fa58e5e6d08d5
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-09 19:29:00 +0800

    Add function for computer choice

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
