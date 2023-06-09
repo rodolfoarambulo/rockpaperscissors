ba08c6999f18e00ecd49a324be7ed5fd40f44c14
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-09 19:55:31 +0800

    Add function to play a single round of the game

--- scripts/game.js
+++ scripts/game.js
@@ -2,10 +2,31 @@ function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissors';
+            return 'scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return 'You lose! Paper beats Rock';
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return 'You win! Rock beats Scissors'
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return 'You lose! Scissors beats Rock';
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return 'You win! Paper beats Rock';
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return 'You lose! Rock beats Scissors';
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return 'You win! Scissors beats Paper';
+    } else {
+        return 'Unknown conditions!';
     }
 }
