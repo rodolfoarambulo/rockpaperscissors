01f42f382d30c4356d06650094b602488fe09bbc
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-19 17:05:41 +0800

    Add running score counter and win conditions

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,9 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const result = document.querySelector('#result');
+const scores = document.querySelector('#scores');
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
@@ -15,29 +21,53 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
         return "It's a tie!";
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        computerScore++;
         return 'You lose! Paper beats Rock';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return 'You win! Rock beats Scissors'
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        computerScore++;
         return 'You lose! Scissors beats Paper';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return 'You win! Paper beats Rock';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        computerScore++;
         return 'You lose! Rock beats Scissors';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        playerScore++;
         return 'You win! Scissors beats Paper';
     } else {
         return 'Unknown conditions!';
     }
 }
 
-const result = document.querySelector('#result');
+function resetScores() {
+    playerScore = 0;
+    computerScore = 0;
+}
+
+function checkScores() {
+    if (playerScore === 5) {
+        alert('Player wins!');
+        resetScores();
+    }
+
+    if (computerScore === 5) {
+        alert('Computer wins!');
+        resetScores();
+    }
+}
+
 
 const btnRock = document.querySelector('#btnRock');
 btnRock.addEventListener(
     'click',
     () => {
         result.textContent = playRound('rock', getComputerChoice());
+        checkScores();
+        scores.textContent = 'Player: ' + playerScore + ' | COM: ' + computerScore;
     },
     false
 );
@@ -47,6 +77,8 @@ btnPaper.addEventListener(
     'click',
     () => {
         result.textContent = playRound('paper', getComputerChoice());
+        checkScores();
+        scores.textContent = 'Player: ' + playerScore + ' | COM: ' + computerScore;
     },
     false
 );
@@ -56,6 +88,8 @@ btnScissors.addEventListener(
     'click',
     () => {
         result.textContent = playRound('scissors', getComputerChoice());
+        checkScores();
+        scores.textContent = 'Player: ' + playerScore + ' | COM: ' + computerScore;
     },
     false
 );
